fde5670e269670a2128f5cb4d7090a651902fe37
Abishekcs <abishekdascs@gmail.com>
2026-02-25 09:32:11 +0530

    Make remove_index safe with if_exists: true

--- db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
+++ db/migrate/20250730205439_remove_index_articles_courses_on_course_id.rb
@@ -1,5 +1,7 @@
-class RemoveIndexArticlesCoursesOnCourseId < ActiveRecord::Migration[7.0]
+class RemoveIndexArticlesCoursesOnCourseId < ActiveRecord::Migration[8.0]
   def change
-    remove_index :articles_courses, name: :index_articles_courses_on_course_id
+    remove_index :articles_courses,
+                 name: :index_articles_courses_on_course_id,
+                 if_exists: true
   end
 end
